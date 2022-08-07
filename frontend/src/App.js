85acf3f6c89adc625119b6e63034e7ea0d62e83e
Adriana Galvez Hurtado <ady@Adrianas-MacBook-Air.local>
2022-08-06 21:49:10 -0500

    holi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,91 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const name = "Minions"
+  const description= "this is a description"
+
+  const jsonData = {
+    "movies" : [
+      {
+        "name": "Fast and Furious: 14",
+        "stars": 5,
+        "description": "this is a description",
+        "image": "string-encoded-image",
+        "preview": "youtube-link.com"
+      },
+    ],
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div>
+    <header>
+      <div class="container">
+        <a href="index.html">
+          <img href="https://cinepolis.com/" class="logo"/>
+        </a> 
+        <nav>
+            <a href="#inicio">Inicio</a>
+            <a href="#sign">Sign In</a>          
+            <a href="#cartelera">Cartelera</a>
+            <a href="#servicios">Servicios</a>
+            <a href="#contactanos">Contactános</a>
+
+          </nav>
+      </div>
+    </header>
+    <main>
+      {/*SECCION INICIO*/}
+      <section id="inicio">
+        <img src=""/>
+        <div class="bloque-inicio">
+          <h1>Bienvenidos a Cinépolis</h1>
+          <p>
+            Lorem ipsum dolor sit amet
+          </p>
+          <a href="#cartelera" class="boton boton-rojo">Ver Cartelera</a>
+        </div>  
+      </section>
+
+      {/*SECCION CARTELERA*/}
+      <section id="cartelera" class="seccion">
+        <div class="container">
+          <div class="row"> {/* Primer renglòn de peliculas */}
+
+            {/* Primer pélicula */}
+            <div class="columna columa-33">
+              <div class="bloque-servicio">
+                <div class="bloque-img-servicio cuadrado-perfecto">
+                  <img src=""/>
+                </div>
+                <div class="bloque-contenido-servicio">
+                  <h3>{name}</h3>
+                  <p>{description}</p>
+                  <a href="#" class="boton boton-blanco">Ver más</a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+          </div>
+        </div>  
+      </section>
+
+
+    </main>
+
+  </div>
+
+  
+
+
+
   );
 }
 
