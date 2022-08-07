bd3d5aa93f8ad0941c32e4d41593387dd6981a0e
erik-dunteman <erikdunteman@gmail.com>
2022-08-06 23:04:14 -0500

    merge

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,56 @@
 const express = require("express");
+var cors = require('cors')
+const db = require("./mockDB");
 const app = express();
+app.use(cors())
 
-const port = 3000;
+const port = 3001;
+
+const movies = [
+  {
+    name: "Fast and Furious: 15",
+    stars: 5,
+    genres: ["action"],
+    description: "The cars are both fast and furious.",
+    image: "string-encoded-image",
+    preview: "https://www.youtube.com/watch?v=FUK2kdPsBws",
+  },
+  {
+    name: "Magic Mike",
+    stars: 4.69,
+    genres: ["romance", "drama"],
+    description: "Mike is magical.",
+    image: "string-encoded-image",
+    preview: "https://www.youtube.com/watch?v=VEhYnUr0EDE",
+  },
+];
+
+app.use(express.json());
 
 // respond with basic movies
 app.get("/movies", (req, res) => {
+  const cache = db.getCache();
+  console.log(cache);
+
   const jsonOut = {
-    movies: [
-      {
-        name: "Fast and Furious: 15",
-        stars: 5,
-        description: "The cars are both fast and furious.",
-        image: "string-encoded-image",
-        preview: "https://www.youtube.com/watch?v=FUK2kdPsBws",
-      },
-      {
-        name: "Magic Mike",
-        stars: 4.69,
-        description: "Mike is magical.",
-        image: "string-encoded-image",
-        preview: "https://www.youtube.com/watch?v=VEhYnUr0EDE",
-      },
-    ],
+    movies: movies,
+    cache: cache,
   };
   res.send(jsonOut);
+});
+
+// handle a "login" session in which we just take in preferences and write to a local db
+app.post("/write/preferences", (req, res) => {
+  const cache = db.getCache();
+
+  cache.username = req.body.username;
+  cache.preferences = req.body.preferences;
+
+  db.setCache(cache);
+
+  console.log("Wrote to cache:", cache)
+
+  res.send(cache);
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState({"movies":[]});
 
   const name = "Minions"
   const description= "this is a description"
   const image="https://assets.cinepolisklic.com/cmsklicia/movieimages/minions/poster_originalsize_250X375.jpg"
 
-  const jsonData = {
-    "movies" : [
-      {
-        "name": "Fast and Furious: 14",
-        "stars": 5,
-        "description": "this is a description",
-        "image": "string-encoded-image",
-        "preview": "youtube-link.com"
-      },
-    ],
+  const fetchData = async () => {
+    const out = await fetch('http://localhost:3001/movies')
+    const json = await out.json()
+    console.log(json)
+    data.movies = json.movies
+    setData(data)
   }
+
+  const handleLogin = async () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        username: username,
+        preferences: {
+          enjoys: [
+            "action",
+            "comedy"
+          ]
+        }
+      })
+    };
+    await fetch('http://localhost:3001/write/preferences', requestOptions)
+    fetchData()
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleLogin()
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
 
   return (
 
@@ -39,6 +66,13 @@ function App() {
       </div>
     </header>
     <main>
+      <h3>Log In</h3>
+      <input
+        value = {username}
+        placeholder="your-username"
+        onChange = {(e) => {setUsername(e.target.value)}}
+        onKeyDown={handleKeyDown} 
+      ></input>
       {/*SECCION INICIO*/}
       <section id="inicio">
         <img src="https://www.cinepolis.com.ar/images/4d/4d-3.jpg"/>
@@ -56,52 +90,22 @@ function App() {
         <div class="container">
           <div class="row"> {/* Primer renglòn de peliculas */}
 
-            {/* Primer pélicula */}
-            <div class="columna columa-33">
-              <div class="bloque-servicio">
-                <div class="bloque-img-servicio cuadrado-perfecto">
-                  <img src={image}/>
+            {data.movies.map(function(movie, i){
+                return (
+                <div class="columna columa-33">
+                  <div class="bloque-servicio">
+                    <div class="bloque-img-servicio cuadrado-perfecto">
+                      <img src={image}/>
+                    </div>
+                    <div class="bloque-contenido-servicio">
+                      <h3>{movie.name}</h3>
+                      <p>{movie.description}</p>
+                      <a href="#" class="boton boton-blanco">Ver más</a>
+                    </div>
+                  </div>
                 </div>
-                <div class="bloque-contenido-servicio">
-                  <h3>{name}</h3>
-                  <p>{description}</p>
-                  <a href="#" class="boton boton-blanco">Ver más</a>
-                </div>
-              </div>
-            </div>
-
-             {/* Segunda pélicula */}
-             <div class="columna columa-33">
-              <div class="bloque-servicio">
-                <div class="bloque-img-servicio cuadrado-perfecto">
-                  <img src={image}/>
-                </div>
-                <div class="bloque-contenido-servicio">
-                  <h3>{name}</h3>
-                  <p>{description}</p>
-                  <a href="#" class="boton boton-blanco">Ver más</a>
-                </div>
-              </div>
-            </div>
-
-             {/* Tercer pélicula */}
-             <div class="columna columa-33">
-              <div class="bloque-servicio">
-                <div class="bloque-img-servicio cuadrado-perfecto">
-                  <img src={image}/>
-                </div>
-                <div class="bloque-contenido-servicio">
-                  <h3>{name}</h3>
-                  <p>{description}</p>
-                  <a href="#" class="boton boton-blanco">Ver más</a>
-                </div>
-              </div>
-            </div>
-
-
-
-
-
+                )
+            })}
 
           </div>
         </div>  
